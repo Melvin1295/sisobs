8b16daefba8cefa26649b53f370ff822f91f242c
mdiazr <mdiaz@honeysoft.pe>
2017-04-15 19:26:37 -0500

    todo va bn

--- public/js/app/pages/controllers.js
+++ public/js/app/pages/controllers.js
@@ -3,6 +3,27 @@
         .controller('PageController',['$window','$scope', '$routeParams','$location','crudService' ,'$filter','$route','$log',
             function($window,$scope, $routeParams,$location,crudService,$filter,$route,$log){
                   $scope.alert=true;              
+                    
+                   $scope.ultimaPublicacion = {};
+                   $scope.publicaciones=[];
+                    //Trae el ultimo registro ordenado por fecha
 
+                $scope.traerUltimo= function(){
+                    crudService.search('publishersUltimo',0,1).then(function (data){
+                        $scope.ultimaPublicacion = data;
+                        console.log('$scope.ultimo');
+                        console.log($scope.ultimo);
+                    });
+                }
+                //trae todos los registros paginados en 15
+                $scope.traerAll= function(){
+                    crudService.search('publishers_all',0,1).then(function (data){
+                        $scope.publicaciones = data.data;
+                        console.log('$scope.todos');
+                        console.log($scope.todos);
+                        console.log(data);
+                    });
+                }
+                    $scope.traerAll();
             }]);
 })();
--- resources/views/pages/form_blog.blade.php
+++ resources/views/pages/form_blog.blade.php
@@ -8,18 +8,18 @@
             	
                 <!-- BLOG POSTS - START -->
                 <div class="col-sm-8">
-                	<h2>Our Blog</h2>
+                	<h2>Publicaciones</h2>
                     <div class="timeline">
                     
                     	<!-- BLOG POST 1 - START -->
-                        <div class="blog-post">
+                        <div class="blog-post" ng-repeat="item in publicaciones">
                         	<div class="blog-info">
-                            	<img src="assets/images/avatar_01.jpg" class="img-responsive" alt="">
+                            	<img src="@{{item.autor.imagen}}" class="img-responsive" alt="">
                                 <div class="date"><div class="number">18</div><div>Dec</div></div>
                             </div>
-                            <h3><a href="blogitem.html">Introducing WordPress 4.0 “Benny”</a></h3>
+                            <h3><a href="blogitem.html">@{{item.titulo}}</a></h3>
                             <div class="date-xs">18/12/2014</div>
-                            <p><img class="img-responsive pull-right" src="assets/images/image_01.JPG" alt="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In venenatis lacus ac semper viverra. Etiam consequat odio sollicitudin metus condimentum, quis hendrerit erat adipiscing. Cras non turpis ut eros varius laoreet a ut felis. Aliquam sodales, purus pulvinar tincidunt congue, elit mauris sodales nisl, consequat iaculis urna enim in lectus. Proin at ornare lacus.</p>
+                            <p><img class="img-responsive pull-right" src="@{{item.imagen}}" alt="">@{{item.descripcion_corta}}</p>
                         	<ul class="blog-tags list-unstyled list-inline">
                                 <li><a href=""><i class="fa fa-tag"></i>book</a></li>
                                 <li><a href=""><i class="fa fa-tag"></i>music</a></li>
@@ -31,62 +31,7 @@
                         </div>
                         <!-- BLOG POST 1 - END -->
                         
-                        <!-- BLOG POST 2 - START -->
-                        <div class="blog-post">
-                        	<div class="blog-info">
-                            	<img src="assets/images/avatar_02.jpg" class="img-responsive" alt="">
-                                <div class="date"><div class="number">09</div><div>Dec</div></div>
-                            </div>
-                            <h3><a href="blogitem.html">Nature Center: Earth Day Festival</a></h3>
-                            <p><img class="img-responsive pull-right" src="assets/images/image_02.JPG" alt="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In venenatis lacus ac semper viverra. Etiam consequat odio sollicitudin metus condimentum, quis hendrerit erat adipiscing. Cras non turpis ut eros varius laoreet a ut felis. Aliquam sodales, purus pulvinar tincidunt congue, elit mauris sodales nisl, consequat iaculis urna enim in lectus. Proin at ornare lacus.</p>
-                        	<ul class="blog-tags list-unstyled list-inline">
-                                <li><a href=""><i class="fa fa-tag"></i>book</a></li>
-                                <li><a href=""><i class="fa fa-tag"></i>music</a></li>
-                                <li><a href=""><i class="fa fa-tag"></i>nature</a></li>
-                                <li><a href=""><i class="fa fa-tag"></i>read</a></li>
-                                <li><a href=""><i class="fa fa-tag"></i>songs</a></li>
-                                <li><a href=""><i class="fa fa-tag"></i>sunshine</a></li>  
-                            </ul>
-                        </div>
-                        <!-- BLOG POST 2 - END -->
-                        
-                        <!-- BLOG POST 3 - START -->
-                        <div class="blog-post">
-                        	<div class="blog-info">
-                            	<img src="assets/images/avatar_03.jpg" class="img-responsive" alt="">
-                                <div class="date"><div class="number">25</div><div>Nov</div></div>
-                            </div>
-                            <h3><a href="blogitem.html">Two New Stores Set to Open in Downtown</a></h3>
-                            <p><img class="img-responsive pull-right" src="assets/images/image_03.JPG" alt="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In venenatis lacus ac semper viverra. Etiam consequat odio sollicitudin metus condimentum, quis hendrerit erat adipiscing. Cras non turpis ut eros varius laoreet a ut felis. Aliquam sodales, purus pulvinar tincidunt congue, elit mauris sodales nisl, consequat iaculis urna enim in lectus. Proin at ornare lacus.</p>
-                        	<ul class="blog-tags list-unstyled list-inline">
-                                <li><a href=""><i class="fa fa-tag"></i>book</a></li>
-                                <li><a href=""><i class="fa fa-tag"></i>music</a></li>
-                                <li><a href=""><i class="fa fa-tag"></i>nature</a></li>
-                                <li><a href=""><i class="fa fa-tag"></i>read</a></li>
-                                <li><a href=""><i class="fa fa-tag"></i>songs</a></li>
-                                <li><a href=""><i class="fa fa-tag"></i>sunshine</a></li>  
-                            </ul>
-                        </div>
-                        <!-- BLOG POST 3 - END -->
-                        
-                        <!-- BLOG POST 4 - START -->
-                        <div class="blog-post">
-                        	<div class="blog-info">
-                            	<img src="assets/images/avatar_04.jpg" class="img-responsive" alt="">
-                                <div class="date"><div class="number">19</div><div>Nov</div></div>
-                            </div>
-                            <h3><a href="blogitem.html">We Have Moved to a New Office</a></h3>
-                            <p><img class="img-responsive pull-right" src="assets/images/image_04.JPG" alt="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In venenatis lacus ac semper viverra. Etiam consequat odio sollicitudin metus condimentum, quis hendrerit erat adipiscing. Cras non turpis ut eros varius laoreet a ut felis. Aliquam sodales, purus pulvinar tincidunt congue, elit mauris sodales nisl, consequat iaculis urna enim in lectus. Proin at ornare lacus.</p>
-                        	<ul class="blog-tags list-unstyled list-inline">
-                                <li><a href=""><i class="fa fa-tag"></i>book</a></li>
-                                <li><a href=""><i class="fa fa-tag"></i>music</a></li>
-                                <li><a href=""><i class="fa fa-tag"></i>nature</a></li>
-                                <li><a href=""><i class="fa fa-tag"></i>read</a></li>
-                                <li><a href=""><i class="fa fa-tag"></i>songs</a></li>
-                                <li><a href=""><i class="fa fa-tag"></i>sunshine</a></li>  
-                            </ul>
-                        </div>
-                        <!-- BLOG POST 4 - END -->
+                       
                         
                     </div>
                     
@@ -110,25 +55,13 @@
                                 <li><a href=""><i class="fa fa-hashtag"></i>Uncategorized</a></li>
                             </ul>
                         </div>
-                        <h3>Recent Posts</h3>
+                        <h3>Publicaciones Recientes</h3>
                         <div class="box posts">
                             <ul class="list-unstyled">
-                                <li><i class="fa fa-chevron-right"></i><a href="">Introducing WordPress 4.0 “Benny”</a><div>27/02/2014</div></li>
-                                <li><i class="fa fa-chevron-right"></i><a href="">Nature Center: Earth Day Festival</a><div>18/02/2014</div></li>
-                                <li><i class="fa fa-chevron-right"></i><a href="">Two New Stores Set to Open in Downtown</a><div>05/02/2014</div></li>
+                                <li ng-repeat="item in publicaciones"><i class="fa fa-chevron-right"></i><a href="">@{{item.titulo}}</a><div>27/02/2014</div></li>
                             </ul>
                         </div>
-                        <h3>Tags</h3>
-                        <div class="box tags">
-                            <ul class="blog-tags list-unstyled list-inline">
-                                <li><a href=""><i class="fa fa-tag"></i>book</a></li>
-                                <li><a href=""><i class="fa fa-tag"></i>music</a></li>
-                                <li><a href=""><i class="fa fa-tag"></i>nature</a></li>
-                                <li><a href=""><i class="fa fa-tag"></i>read</a></li>
-                                <li><a href=""><i class="fa fa-tag"></i>songs</a></li>
-                                <li><a href=""><i class="fa fa-tag"></i>sunshine</a></li>                            
-                            </ul>
-                        </div>
+                       
                     </div>
                 </div>
                 <!-- SIDEBAR - END -->
